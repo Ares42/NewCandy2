68f1a34eb6c49274bef3bd7265f8c56dfff21336
luke_solomon@me.com <lukesolomon@Lukes-MacBook-Pro.local>
2014-09-21 19:41:30 -0700

    Initial Commit

---
+++ NewCandy2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewCandy2
+//
+//  Created by Luke Solomon on 9/21/14.
+//  Copyright (c) 2014 Luke Solomon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewCandy2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewCandy2
+//
+//  Created by Luke Solomon on 9/21/14.
+//  Copyright (c) 2014 Luke Solomon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
